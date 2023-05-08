97b688640fbdc3b4fd7edfe512cf31605956b0ab
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-08 13:59:29 +0200

    feat (frontend): hello world json request

---
+++ frontend/src/components/Echo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p>{{ data }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+
+export default {
+    data() {
+        return {
+            data: '',
+        };
+    },
+    async mounted() {
+        try {
+            const response = await fetch("http://localhost:8080", {});
+            const data = await response.json();
+            this.data = data;
+        } catch (error) {
+            if (error instanceof Error) {
+                this.data = error.message;
+            }
+            console.log(error);
+        }
+    },
+};
+</script>
