10d5a158d8a12b2c92813591aba0bc8cce9f0976
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-09 10:15:05 +0200

    add (frontend): location model

---
+++ frontend/src/models/location.ts
@@ -0,0 +1,9 @@
+export interface Location {
+    id: string;
+    name: string;
+    location_type: string;
+    attributes: string[];
+    noise: string;
+    address: string;
+    coordinates: string[];
+}
