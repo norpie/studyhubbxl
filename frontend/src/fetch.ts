ce4e624248c69274770e90d8ae388e49b813bbfd
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-12 12:13:01 +0200

    feature (frontend): get, post added.

--- frontend/src/fetch.ts
+++ frontend/src/fetch.ts
@@ -1,21 +1,27 @@
-async function get<T>(url: string): Promise<T | string> {
-    await fetch(url)
-        .then(response => {
-            response.json().then(json => {
-                if (json["result"] != null && json["result"] != "") {
-                    let object = json["result"] as T;
-                    return object;
-                }
-                return json["error"];
-            }).catch(error => {
-                return "Failed to get json: " + error.message;
-            });
+// https://eckertalex.dev/blog/typescript-fetch-wrapper
+export async function http<T>(path: string, config: RequestInit): Promise<T> {
+    const request = new Request(path, config)
+    const response = await fetch(request)
 
-        })
-        .catch(error => {
-            return "Failed to fetch: " + error.message;
-        });
-    return ""
+    if (!response.ok) {
+        throw new Error(/*{ name: response.status, message: response.statusText }*/"Error fetching.")
+    }
+
+    // may error if there is no body, return empty array
+    let json = await response.json().then(json => { return json }).catch(error => { throw error });
+    let object = json.result;
+    if (object == null) {
+        throw new Error(json.error);
+    }
+    return object
 }
 
-export default get;
+export async function get<T>(path: string, config?: RequestInit): Promise<T> {
+    const init = { method: 'get', ...config }
+    return await http<T>(path, init)
+}
+
+export async function post<T, U>(path: string, body: T, config?: RequestInit): Promise<U> {
+    const init = { method: 'post', body: JSON.stringify(body), ...config }
+    return await http<U>(path, init)
+}
