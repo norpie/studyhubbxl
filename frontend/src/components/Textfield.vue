7fe21b77ea3b6dae6205d85d1d74e8f04620ecb2
ThatssoBryana <132075497+ThatssoBryana@users.noreply.github.com>
2023-05-12 16:22:45 +0200

    feature-textfield

---
+++ frontend/src/components/Textfield.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+export default {
+    props: {p:String},
+
+    data() {return{
+        mail: 'Enter your email',
+        password:'Enter your password'}},
+ }
+
+</script>
+
+
+<template>
+<p> Mail </p>
+<input type="text"  class="mail" v-model="mail" placeholder="Enter your email" />
+<p> Password </p>
+<input type="text"  class="password" v-model="password" placeholder="Enter your email" />
+</template>
+
+<style scoped>
+p{font-weight: bold;}
+.mail{ 
+    color: gray;
+    border-style: none;
+    background-color:white;
+}
+.password{ 
+    color:gray;
+    border-style: none;
+    background-color: white;
+    }
+</style>
