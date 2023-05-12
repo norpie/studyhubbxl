0c596f204388b2659fdb715c7256bed996bfd2d3
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-12 13:28:37 +0200

    fix (frontend): actually assign id to the attribute and refactored

--- frontend/src/components/Checkbox.vue
+++ frontend/src/components/Checkbox.vue
@@ -12,26 +12,21 @@ export default {
     }
 }
 </script>
+
 <template>
     <div>
         <label for="checkbox">{{ label }}</label>
-        <input type="checkbox" id="checkbox" v-model="checked" />
+        <input type="checkbox" class="checkbox" :id="id" v-model="checked" />
     </div>
 </template>
 
-
-
 <style scoped>
-input:checked {
-    accent-color: grey;
+.checkbox:checked {
     background-color: black;
-    height: 10px;
-    border-radius: 2px;
-    width: 10x;
     appearance: none;
 }
 
-input {
+.checkbox {
     accent-color: grey;
     background-color: white;
     border-color: black;
@@ -39,18 +34,9 @@ input {
     border-radius: 2px;
     border-width: 1px;
     margin: 0px;
-    margin-left:5px ;
+    margin-left: 5px;
     height: 10px;
     width: 10px;
     appearance: none;
-
-}
-
-label {
-
-    align-items: center;
 }
 </style>
-
-
-
