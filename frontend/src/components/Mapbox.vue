baf36c9c2edde5d22f4d2bd216d06f218c9c4c86
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-14 16:24:32 +0200

    added Mapbox component

---
+++ frontend/src/components/Mapbox.vue
@@ -0,0 +1,32 @@
+<template>
+    <mapbox-map
+        :accessToken="accessToken"
+        :center="center"
+        :zoom="zoom"
+        :maxBounds="bounds"
+        :auto-resize=true>
+        <slot>
+        </slot>
+        <mapbox-navigation-control position="top-left" />
+        <!--<mapbox-geolocate-control />-->
+    </mapbox-map>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        accessToken: String,
+    },
+    data() {
+        return {
+            center: [4.363567, 50.844674],
+            zoom: 11.5,
+            bounds: [
+                [4.15, 50.75], [4.6, 50.95]
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped></style>
