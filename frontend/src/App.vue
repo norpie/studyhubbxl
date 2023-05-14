91bfd5b843a8d6128dff6038739beb959893d35e
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-14 16:26:50 +0200

    fix (frontend): default style for app so that it takes up entire page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
-import Button from './components/Button.vue'
 </script>
 
 <template>
-    <Button/>
 </template>
+
+<style>
+html {
+    width: 100%;
+    height: 100%;
+}
+
+body {
+    width: 100%;
+    height: 100%;
+    margin: 0 0 0 0;
+}
+
+#app {
+    width: 100%;
+    height: 100%;
+}
+</style>
