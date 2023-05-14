d6cc31d52dea03a87322d5c9973907d3f967aebe
SimonVerbandt <simon.verbandt@student.ehb.be>
2023-05-14 14:39:15 +0200

    Code redo

--- backend/src/routes/location.rs
+++ backend/src/routes/location.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, Responder, web::{Query, Json, Path}, post, HttpRequest, HttpResponse};
+use actix_web::{get, Responder, web::{Query, Json, Path}, post};
 use serde::Deserialize;
 use uuid::Uuid;
 
@@ -22,34 +22,12 @@ struct Filter{
 
 //Request locations by search or filter or both
 #[post("/locations")]
-async fn post_location(query: Query<LocationQuery>, filter: Json<Filter>, request: HttpRequest) -> impl Responder {
-    //Check the access token of the user with the ones corresponding to our database (surreal db)
-    if let Some(token) = request.headers().get("Authorization"){
-           let token = token.to_str().unwrap();
-            if token = "on of our tokens"{
-                let response = ApiResponse::new(" ");
-              return HttpResponse::Ok().json(response)
-            
-           }else{
-            return HttpResponse::Unauthorized().body("Invalid token/No token")
-           }
-    }
-   
+async fn post_location(query: Query<LocationQuery>, filter: Json<Filter>) -> impl Responder {
+    ApiResponse::new(" ")
 }
 
 //Get specific location
 #[get("/location/{id}")]
-async fn get_location(id: Path<Uuid>, request: HttpRequest) -> impl Responder{
-     //Check the access token of the user with the ones corresponding to our database (surreal db)
-     if let Some(token) = request.headers().get("Authorization"){
-        let token = token.to_str().unwrap();
-         if token = "on of our tokens"{
-             let response = ApiResponse::new(" ");
-           return HttpResponse::Ok().json(response)
-         
-        }else{
-         return HttpResponse::Unauthorized().body("Invalid token/No token")
-        }
- }
-
+async fn get_location(id: Path<Uuid>) -> impl Responder{
+    ApiResponse::new(" ")
 }
