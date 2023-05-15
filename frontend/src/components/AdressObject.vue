c7e2cdbdf4db2d51b6f440306fc4cdbf0fe63242
AbdelillahEl <abdelpro19@gmail.com>
2023-05-15 15:06:10 +0200

    feature-lijst-adresscard

---
+++ frontend/src/components/AdressObject.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="address-object">
+      <div class="address-rectangle">{{ label }}</div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "AddressObject",
+    props: {
+      label: {
+        type: String,
+        required: true,
+      },
+    },
+  });
+  </script>
+  
+  <style scoped>
+  .address-object {
+    width: 200px;
+    height: 100px;
+    margin: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .address-rectangle {
+    width: 100%;
+    height: 100%;
+    background-color: lightgray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: black;
+    font-weight: bold;
+  }
+  </style>
+  
---
+++ frontend/src/components/Lijst.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="address-list-wrapper">
+      <div class="address-list">
+        <slot></slot>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: "Lijst",
+  };
+  </script>
+  
+  <style scoped>
+  .address-list-wrapper {
+    height: 330px;
+    overflow-y: scroll;
+    display: flex;
+    justify-content: center;
+  }
+  
+  .address-list {
+    width: 200px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  </style>
