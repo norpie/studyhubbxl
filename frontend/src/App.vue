29364de99a9d781bce53571f0f9b0ae3fae5ca1a
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-16 12:34:26 +0200

    changed example for button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,41 +1,11 @@
 <script setup lang="ts">
 import Button from './components/Button.vue'
 
-function login() {
-    
+function test() {
+    console.log("test");
 }
-
-function logout() {
-    
-}
-
-function delAccount() {
-    
-}
-
-function forgotPsw() {
-    
-}
-
-function changePsw() {
-    
-}
-
-
 </script>
 
 <template>
-    
-    <Button label="login" @event="login"/>
-    <br>
-    <Button label = "logout" @event="logout"/>
-    <br>
-    <Button label = "delete account" @event="delAccount"/>
-    <br>
-    <Button label = "forgot passwoord" @event="forgotPsw"/>
-    <br>
-    <Button label = "change passwoord " @event="changePsw"/>
+    <Button label="test" @click="test"/>
 </template>
-
-
-
