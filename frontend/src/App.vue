2b8f8eea5fdca9b4e9dbfbe7883184638015263d
Misterkrok <128594657+Misterkrok@users.noreply.github.com>
2023-05-16 10:38:40 +0200

    feature-button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,40 @@
 <script setup lang="ts">
 import Button from './components/Button.vue'
 
+function login() {
+    
+}
+
+function logout() {
+    
+}
+
+function delAccount() {
+    
+}
+
+function forgotPsw() {
+    
+}
+
+function changePsw() {
+    
+}
 
 
 </script>
 
 <template>
-    <Button label="blah" click="test()"/>
+    
+    <Button label="login" @event="login"/>
     <br>
-    <button label = "u moeder" click="test()"/>
+    <Button label = "logout" @event="logout"/>
+    <br>
+    <Button label = "delete account" @event="delAccount"/>
+    <br>
+    <Button label = "forgot passwoord" @event="forgotPsw"/>
+    <br>
+    <Button label = "change passwoord " @event="changePsw"/>
 </template>
 
 
--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,32 +1,44 @@
-<script lang="ts">
-export default {
-    props: {
-        label: String,
-        
-    },
-}
-
-</script>
-
 <template>
-    <button >{{ label }}</button>
+    <button @click="$emit('click')">{{ label }}</button>
 </template>
 
 <style scoped>
 button {
     font-weight: bold;
     color: white;
-    background-color: blue;
+    background-color: grey;
     opacity: 60%;
     border: none;
     padding: 5px 20px 5px 20px;
-    border-radius: 8px;
+    border-radius: 10px;
+    margin: 5px;
+
 }
 
 button:active {
-    background-color: blueviolet;
+    background-color: brown;
 
 }
 </style>
+<script lang="ts">
 
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        label: String,
+        event:String,
+    },
+   
+  emits: {
+    click: null,
+},
+methods:{
+submit(){
+    this.$emit('click')
+    
+}
+}
+})
+</script>
 
