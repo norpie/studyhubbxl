70c91cd870f235086295c2d5436e3344deeb3d02
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-16 12:38:06 +0200

    merged dev App.vue into feature to resolve conflict

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,5 +7,23 @@ function test() {
 </script>
 
 <template>
-    <Button label="test" @click="test"/>
+    <Button label="test" @click="test" />
 </template>
+
+<style>
+html {
+    width: 100%;
+    height: 100%;
+}
+
+body {
+    width: 100%;
+    height: 100%;
+    margin: 0 0 0 0;
+}
+
+#app {
+    width: 100%;
+    height: 100%;
+}
+</style>
