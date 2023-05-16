cd00d4a78c8e570f8918c5975e481801589686bf
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-16 21:24:34 +0200

    went back to default App.vue to resolve conflict

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
 import Button from './components/Button.vue'
-import Textfield from './components/Textfield.vue';
+
+function test() {
+    console.log("test");
+}
 </script>
 
 <template>
-    <textfield label="Email" placeholder="Enter your email"/>
-    <br>
-    <textfield label="Password" placeholder="Enter your password"/>
+    <Button label="test" @click="test"/>
 </template>
+
+<style>
+html {
+    width: 100%;
+    height: 100%;
+}
+
+body {
+    width: 100%;
+    height: 100%;
+    margin: 0 0 0 0;
+}
+
+#app {
+    width: 100%;
+    height: 100%;
+}
+</style>
