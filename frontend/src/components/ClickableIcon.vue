bb91c4f6acb03c3826fcd298bbd640150d0b9ba0
AbdelillahEl <abdelpro19@gmail.com>
2023-05-17 12:51:27 +0200

    feature-clickableicon
    Just add a png img in between the button and it will work

---
+++ frontend/src/components/ClickableIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="clickable-icon" @click="handleClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "ClickableIcon",
+  data() {
+    return {
+      isClicked: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.isClicked = !this.isClicked;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.clickable-icon {
+  padding: 0;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  
+}
+.clickable-icon :active{
+  background-color: yellow;
+}
+.clickable-icon img {
+  height: 24px;
+  width: 24px;
+}
+</style>
