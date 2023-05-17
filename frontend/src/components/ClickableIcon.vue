cc8811d1710bd60c6c407455a8ae480bb914a55a
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-17 15:11:19 +0200

    formatting

--- frontend/src/components/ClickableIcon.vue
+++ frontend/src/components/ClickableIcon.vue
@@ -1,38 +1,40 @@
 <template>
-  <button class="clickable-icon" @click="handleClick">
-    <slot></slot>
-  </button>
+    <button class="clickable-icon" @click="handleClick">
+        <slot></slot>
+    </button>
 </template>
 
 <script lang="ts">
 export default {
-  name: "ClickableIcon",
-  data() {
-    return {
-      isClicked: false,
-    };
-  },
-  methods: {
-    handleClick() {
-      this.isClicked = !this.isClicked;
+    name: "ClickableIcon",
+    data() {
+        return {
+            isClicked: false,
+        };
     },
-  },
+    methods: {
+        handleClick() {
+            this.isClicked = !this.isClicked;
+        },
+    },
 };
 </script>
 
 <style scoped>
 .clickable-icon {
-  padding: 0;
-  border: none;
-  background: transparent;
-  cursor: pointer;
-  
+    padding: 0;
+    border: none;
+    background: transparent;
+    cursor: pointer;
+
 }
-.clickable-icon :active{
-  background-color: yellow;
+
+.clickable-icon :active {
+    background-color: yellow;
 }
+
 .clickable-icon img {
-  height: 24px;
-  width: 24px;
+    height: 24px;
+    width: 24px;
 }
 </style>
