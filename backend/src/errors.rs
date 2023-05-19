c0281010cc40df86370f1df2cdb3162a624b25b8
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-05-19 18:01:20 +0200

    fixed the thing asked of me

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -1,13 +1,17 @@
-use actix_web::{error, HttpResponse, http::{header::ContentType, StatusCode}, ResponseError, Responder};
+use actix_web::{
+    body::BoxBody,
+    error,
+    http::{header::ContentType, StatusCode},
+    HttpResponse, Responder, ResponseError,
+};
 use derive_more::{Display, Error};
 
-
 use crate::models::ApiResponse;
-    
+
 #[derive(Debug, Display, Error)]
-pub enum UserError{
+pub enum UserError {
     #[display(fmt = "A validation error occurred on field: {}", field)]
-    ValidationError{ field: String},
+    ValidationError { field: String },
     #[display(fmt = "An internal error occurred. Please try again later")]
     InternalError,
     #[display(fmt = "Wrong password")]
@@ -16,47 +20,48 @@ pub enum UserError{
     TooManyRequests,
 }
 
-impl error::ResponseError for UserError{
+impl error::ResponseError for UserError {
     fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
-            HttpResponse::build(self.status_code())
+        HttpResponse::build(self.status_code())
             .insert_header(ContentType::html())
             .body(self.to_string())
     }
-    
+
     fn status_code(&self) -> StatusCode {
         match *self {
             UserError::ValidationError { .. } => StatusCode::BAD_REQUEST,
             UserError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
-            UserError::WrongPasswordOrUsername => StatusCode:: UNAUTHORIZED,
+            UserError::WrongPasswordOrUsername => StatusCode::UNAUTHORIZED,
             UserError::TooManyRequests => StatusCode::TOO_MANY_REQUESTS,
         }
     }
 }
 
-impl <T, Serialize> Responder for Result<ApiResponse<T>, UserError>{
-    type Error = actix_web::Error;
-    type Future = actix_web::dev::Ready<Result<HttpResponse, Self::Error>>;
+impl Responder for UserError {
+    type Body = BoxBody;
 
-    fn respond_to(self, _: &actix_web::HttpRequest) -> Self::Future{
-        match self {
-            Ok(api_response) =>{
-                let response = HttpResponse::Ok()
-                .insert_header(("Access-Control-Allow-Origin", "http://localholst::8080"))
-                .content_type(ContentType::json())
-                .body(serde_json::to_string(&api_response).unwrap());
-            actix_web::dev::ready(Ok(response))
-            }
-            Err(user_error) => {
-                let code = user_error.status_code().as_u16();
-                let msg = user_error.to_string();
-                let error_response = ApiResponse::error(code as u8, msg);
-                let response = HttpResponse::build(user_error.status_code())
-                    .insert_header(ContentType.json())
-                    .body(serde_json::to_string(&error_response).unwrap());
-                actix_web::dev::ready(Ok(response))
-            }
-        }
+    fn respond_to(self, req: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+        ApiResponse::<String>::error(self.status_code().as_u16(), self.to_string()).respond_to(req)
     }
 
-   
+    //fn respond_to(self, _: &actix_web::HttpRequest) -> Self::Future{
+    //    match self {
+    //        Ok(api_response) =>{
+    //            let response = HttpResponse::Ok()
+    //            .insert_header(("Access-Control-Allow-Origin", "http://localholst::8080"))
+    //            .content_type(ContentType::json())
+    //            .body(serde_json::to_string(&api_response).unwrap());
+    //        actix_web::dev::ready(Ok(response))
+    //        }
+    //        Err(user_error) => {
+    //            let code = user_error.status_code().as_u16();
+    //            let msg = user_error.to_string();
+    //            let error_response = ApiResponse::error(code as u8, msg);
+    //            let response = HttpResponse::build(user_error.status_code())
+    //                .insert_header(ContentType.json())
+    //                .body(serde_json::to_string(&error_response).unwrap());
+    //            actix_web::dev::ready(Ok(response))
+    //        }
+    //    }
+    //}
 }
