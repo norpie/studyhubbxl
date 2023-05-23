044b82b25c50c896060b30cd983a6c644727cfbd
AbdelillahEl <abdelpro19@gmail.com>
2023-05-23 09:10:44 +0200

    added identifier to location

--- database/Location.sql
+++ database/Location.sql
@@ -1,8 +1,8 @@
-DEFINE TABLE locations SCHEMAFULL;
-DEFINE FIELD id ON TABLE locations TYPE string ASSERT $value != NONE;
-DEFINE FIELD name ON TABLE locations TYPE string ASSERT $value != NONE ;
-DEFINE FIELD location_type ON TABLE locations TYPE string ASSERT $value != NONE;
-DEFINE FIELD attributes ON TABLE locations TYPE string ASSERT $value != NONE;
-DEFINE FIELD noise ON TABLE locations TYPE string ASSERT $value != NONE;
-DEFINE FIELD address ON TABLE locations TYPE string ASSERT $value != NONE ;
-DEFINE FIELD coordinates ON TABLE locations TYPE int ASSERT $value != NONE;
+DEFINE TABLE location SCHEMAFULL;
+DEFINE FIELD identifier ON TABLE location TYPE string ASSERT $value != NONE;
+DEFINE FIELD name ON TABLE location TYPE string ASSERT $value != NONE;
+DEFINE FIELD location_type ON TABLE location TYPE string ASSERT $value != NONE;
+DEFINE FIELD attributes ON TABLE location TYPE array ASSERT $value != NONE;
+DEFINE FIELD noise ON TABLE location TYPE string ASSERT $value != NONE;
+DEFINE FIELD address ON TABLE location TYPE string ASSERT $value != NONE;
+DEFINE FIELD coordinates ON TABLE location TYPE array ASSERT $value != NONE;
