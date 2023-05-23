dd3b15a338537ef051eebc7ec8b7e341ac30d137
Misterkrok <128594657+Misterkrok@users.noreply.github.com>
2023-05-23 09:29:08 +0200

    renamed some parameters added filter search location

--- backend/src/v1/location.rs
+++ backend/src/v1/location.rs
@@ -1,20 +1,23 @@
 use actix_web::{
     get, post,
     web::{self, Data, Json, Path, Query},
-    Scope,
+    Either, Scope,
 };
 use serde::Deserialize;
 use surrealdb::{engine::remote::ws::Client, Surreal};
 use uuid::Uuid;
 
-use crate::{error::Result, models::Location};
+use crate::{
+    error::Result,
+    models::{Coordinates, Location},
+};
 use crate::{error::UserError, models::ApiResponse};
 
 #[derive(Debug, Deserialize)]
 struct Search {
     search: Option<String>,
-    top: Option<u8>,
-    skip: Option<u8>,
+    limit: Option<u8>,
+    start: Option<u8>,
     coordinates_only: Option<bool>,
 }
 
@@ -26,14 +29,57 @@ struct Filter {
 }
 
 //Request locations by search or filter or both
-#[post("/")]
+#[post("")]
 async fn filter_search_locations(
     db: Data<Surreal<Client>>,
     search: Query<Search>,
     filter: Json<Filter>,
-) -> Result<ApiResponse<Vec<Location>>> {
-    let query = db.query("");
-    return Ok(ApiResponse::new(Vec::new()));
+) -> Result<Either<ApiResponse<Vec<Location>>, ApiResponse<Vec<Coordinates>>>> {
+    let start = if let Some(start) = search.start {
+        start
+    } else {
+        0
+    };
+    let limit = if let Some(limit) = search.limit {
+        limit
+    } else {
+        20
+    };
+    let sql = "SELECT * FROM location WHERE noise IN $noise AND attributes CONTAINSALL $attributes AND location_type IN $location_types LIMIT $limit START $start";
+    let mut query = db.query(sql);
+    query = query.bind(("noise", &filter.noise));
+    query = query.bind(("attributes", &filter.attributes));
+    query = query.bind(("location_types", &filter.location_types));
+    query = query.bind(("start", start));
+    query = query.bind(("limit", limit));
+    let query_result = query.await;
+    match query_result {
+        Ok(mut response) => {
+            let parse_result: surrealdb::Result<Vec<Location>> = response.take(0);
+            match parse_result {
+                Ok(locations) => {
+                    // TODO: impl search with levenshtein
+                    if search.coordinates_only.is_some() && search.coordinates_only.unwrap() {
+                        let mut coords = Vec::new();
+                        for location in locations {
+                            coords.push(location.coords());
+                        }
+                        Ok(Either::Right(ApiResponse::new(coords)))
+                    } else {
+                        Ok(Either::Left(ApiResponse::new(locations)))
+                    }
+                }
+                Err(err) => {
+                    println!("error inner: {:#?}", err);
+                    Err(UserError::InternalError)
+                }
+            }
+        }
+        Err(err) => {
+            println!("error outer: {:#?}", err);
+            Err(UserError::InternalError)
+        }
+    }
 }
 
 //Get specific location
@@ -43,7 +89,7 @@ async fn get_location(
     id: Path<Uuid>,
 ) -> Result<ApiResponse<Option<Location>>> {
     let query_result = db
-        .query("SELECT * FROM location WHERE '$id'")
+        .query("SELECT * FROM location WHERE '$id' LIMIT 1")
         .bind(("$id", id.to_string()))
         .await;
     match query_result {
@@ -51,10 +97,16 @@ async fn get_location(
             let parse_result = response.take(0);
             match parse_result {
                 Ok(location) => Ok(ApiResponse::new(location)),
-                Err(err) => Err(UserError::InternalError),
+                Err(err) => {
+                    println!("error inner: {:#?}", err);
+                    Err(UserError::InternalError)
+                }
             }
         }
-        Err(err) => Err(UserError::InternalError),
+        Err(err) => {
+            println!("error outer: {:#?}", err);
+            Err(UserError::InternalError)
+        }
     }
 }
 
