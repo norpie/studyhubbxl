f8ceba9898630c8f6d493adf46dadba6766a5136
Ibrahim Iqbal <ibrahim.iqbal@student.ehb.be>
2023-05-24 11:47:59 +0200

    removed id from attribute migration

--- database/Attribute.sql
+++ database/Attribute.sql
@@ -1,4 +1,3 @@
 DEFINE TABLE attributes SCHEMAFULL;
-DEFINE FIELD id ON TABLE attributes TYPE string ASSERT $value != NONE;
 DEFINE FIELD path ON TABLE attributes TYPE string ASSERT $value != NONE ;
 DEFINE FIELD display_name ON TABLE attributes TYPE string ASSERT $value != NONE;
