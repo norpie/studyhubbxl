a1c0d4b330ff79457e85d38a78e7c2855565b8ad
Ibrahim Iqbal <ibrahim.iqbal@student.ehb.be>
2023-05-24 11:37:29 +0200

    match for different filters

--- backend/src/v1/filter.rs
+++ backend/src/v1/filter.rs
@@ -1,3 +1,5 @@
+use std::collections::hash_map::IntoIter;
+
 use actix_web::{
     get,
     web::{self, Data, Path},
@@ -15,7 +17,12 @@ async fn get_filters(
     db: Data<Surreal<Client>>,
     filter_type: Path<String>,
 ) -> Result<ApiResponse<Vec<String>>> {
-    Err(UserError::InternalError)
+    match filter_type.as_str() {
+        "noise" => Err(UserError::InternalError),
+        "type" => Err(UserError::InternalError),
+        "attributes" => Err(UserError::InternalError),
+        _ => Err(UserError::InternalError),
+    }
 }
 
 pub fn scope() -> Scope {
