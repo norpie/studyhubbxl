860bb51636f2003392f343439ee6f18e3211b5ee
Ibrahim Iqbal <ibrahim.iqbal@student.ehb.be>
2023-05-24 09:22:59 +0200

    button code verwijderd van search-panel

--- frontend/src/Search-panel.vue
+++ frontend/src/Search-panel.vue
@@ -1,22 +1,4 @@
-<script lang="ts">
-export default {
-    data() {
-        return {
-            count: 0
-        }
-    }
-}
-</script>
 
-<template>
-    <button @click="count++">Count is: {{ count }}</button>
-</template>
-
-<style scoped>
-button {
-    font-weight: bold;
-}
-</style>
 /*
 -textbalk
 - een search icontje
