75109ea373b07facb9664586e4c8421a99c4db03
ThatssoBryana <132075497+ThatssoBryana@users.noreply.github.com>
2023-05-25 17:45:26 +0200

    make sure checkbox is before label

--- frontend/src/components/Checkbox.vue
+++ frontend/src/components/Checkbox.vue
@@ -14,13 +14,17 @@ export default {
 </script>
 
 <template>
-    <div>
+    <div class="checkbox-wrapper">
         <label for="checkbox">{{ label }}</label>
         <input type="checkbox" class="checkbox" :id="id" v-model="checked" />
     </div>
 </template>
 
 <style scoped>
+
+.checkbox-wrapper{
+    display: inline;
+}
 .checkbox:checked {
     background-color: black;
     appearance: none;
@@ -33,10 +37,12 @@ export default {
     border-style: solid;
     border-radius: 2px;
     border-width: 1px;
-    margin: 0px;
+    margin-top:5px;
     margin-left: 5px;
+    margin-right: 5px;
     height: 10px;
     width: 10px;
     appearance: none;
+    float: left;
 }
 </style>
