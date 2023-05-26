59fd5b7ce33e4e8a289fbe6666e9e5d5fd5bf71c
Ibrahim Iqbal <ibrahim.iqbal@student.ehb.be>
2023-05-26 18:51:07 +0200

    cleanup and adding prerequisites

--- backend/src/v1/users/misc.rs
+++ backend/src/v1/users/misc.rs
@@ -1,32 +1,31 @@
 use actix_web::{
     get,
-    web::{self, Path},
-    Responder, Scope,
+    web::{self, Data},
+    HttpRequest, Scope,
 };
-use uuid::Uuid;
+use surrealdb::{engine::remote::ws::Client, Surreal};
 
+use crate::error::{Result, UserError};
 use crate::models::ApiResponse;
-use crate::error::Result;
 
 //Get reset password page
 #[get("/reset/{id}")]
-async fn reset_password(id: Path<Uuid>) -> Result<ApiResponse<& 'static str>> {
-    let bool = true;
-    if bool{
-        return Ok(ApiResponse::new(""));
-    }
-    Err(crate::error::UserError::WrongPasswordOrUsername)
+async fn reset_password(
+    db: Data<Surreal<Client>>,
+    req: HttpRequest,
+) -> Result<ApiResponse<&'static str>> {
+    let id = super::parse_id(req);
+    Err(UserError::WrongPasswordOrUsername)
 }
 
 //Get delete user page
 #[get("/delete/{id}")]
-async fn delete_account(id: Path<Uuid>) -> Result<ApiResponse<& 'static str>> {
-    let bool = true;
-    if bool{
-        return Ok(ApiResponse::new(""))
-    }
-    Err(crate::error::UserError::WrongPasswordOrUsername)
-
+async fn delete_account(
+    db: Data<Surreal<Client>>,
+    req: HttpRequest,
+) -> Result<ApiResponse<&'static str>> {
+    let id = super::parse_id(req);
+    Err(UserError::WrongPasswordOrUsername)
 }
 
 pub fn scope() -> Scope {
