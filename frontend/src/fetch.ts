a01cee2d627be618e2a6137de37c55b2978f71a1
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-06-01 22:40:40 +0200

    changed the results fetch to do state

--- frontend/src/fetch.ts
+++ frontend/src/fetch.ts
@@ -16,8 +16,56 @@ export async function http<T>(path: string, config: RequestInit): Promise<T> {
     return object
 }
 
+export async function rawhttp<T>(request: Request): Promise<T> {
+    const response = await fetch(request)
+
+    if (!response.ok) {
+        throw new Error(/*{ name: response.status, message: response.statusText }*/"Error fetching.")
+    }
+
+    // may error if there is no body, return empty array
+    let json = await response.json().then(json => { return json }).catch(error => { throw error });
+    let object = json.result;
+    if (object == null) {
+        throw new Error(json.error);
+    }
+    return object
+}
+
+export async function loginpost<T>(url: string, body: Object): Promise<T> {
+    const body_string = JSON.stringify(body);
+    const length = (new TextEncoder().encode(body_string)).length;
+    const request = new Request(url, {
+        method: 'POST',
+        body: body_string,
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Content-Length': length.toString(),
+        },
+    })
+    try {
+        const response = await fetch(request);
+        if (!response.ok) {
+            let json = await response.json().then(json => { return json }).catch(error => { throw error });
+            throw json["error"];
+        }
+
+        // may error if there is no body, return empty array
+        let json = await response.json().then(json => { return json }).catch(error => { throw error });
+        let object = json.result;
+        return object
+    } catch (error) {
+        throw "Internal error, try again later.";
+    }
+}
+
 export async function get<T>(path: string, config?: RequestInit): Promise<T> {
     const init = { method: 'get', ...config }
+    return await http<T>(path, init)
+}
+
+export async function delete_<T>(path: string, config?: RequestInit): Promise<T> {
+    const init = { method: 'delete', ...config }
     return await http<T>(path, init)
 }
 
--- frontend/src/results.ts
+++ frontend/src/results.ts
@@ -1,5 +1,11 @@
-import { post } from "@/fetch";
+import { get, rawhttp } from "@/fetch";
+import { store } from "@/store";
+import mapboxgl from "mapbox-gl";
+import { createApp } from "vue";
+import type Coordinate from "./models/coordinate";
+import type FilterItem from "./models/filteritem";
 import type Location from "./models/location";
+//import Card from '@/components/Card'
 
 export async function showResults(start: number, refresh: boolean): Promise<Location[]> {
     //attributes
@@ -8,47 +14,84 @@ export async function showResults(start: number, refresh: boolean): Promise<Loca
     attribute_checkboxes.forEach((checkbox) => {
         attributes.push(checkbox.id);
     });
-    console.log(attributes);
     //location type
     let location_types_checkboxes = document.querySelectorAll(".location_type:checked");
     let location_types = [] as string[];
     location_types_checkboxes.forEach((checkbox) => {
         location_types.push(checkbox.id);
     });
-    console.log(location_types);
     //noise
     let noise_checkboxes = document.querySelectorAll(".noise:checked");
     let noise = [] as string[];
     noise_checkboxes.forEach((checkbox) => {
         noise.push(checkbox.id);
     });
-    console.log(noise);
+
+    const default_location_types = await get<FilterItem[]>('http://localhost:8080/api/v1/filters/location_type');
+    const default_noises = await get<FilterItem[]>('http://localhost:8080/api/v1/filters/noise');
+
+    if (location_types.length == 0) {
+        location_types = default_location_types.map(i => i.identifier);
+    }
+
+    if (noise.length == 0) {
+        noise = default_noises.map(i => i.identifier);
+    }
 
     //check the textbox
+    let search = "";
     let possible_input = document.getElementById("search-box");
-    if (possible_input == null) {
-        return [];
+    if (possible_input != null) {
+        search = (possible_input as HTMLInputElement).value;
     }
-    let search = (possible_input as HTMLInputElement).value;
+    const body =
+    {
+        search: search,
+        limit: 10000,
+        start: start,
+        coordinates_only: false,
+        location_types: location_types,
+        attributes: attributes,
+        noise: noise
+    };
+    const url = 'http://localhost:8080/api/v1/locations';
 
-    if (refresh) {
-
-        let pins = await post<Object, Location[]>('http://localhost:8080/api/v1/locations?limit=9999999&coordinates_only=true&search=' + search + '&start=' + start, {
-            'location_types': location_types,
-            'attributes': attributes,
-            'noise': noise
-        });
-        //TODO: pins op map krijgen
-    }
+    //TODO: pins op map krijgen
     try {
-        let results = await post<Object, Location[]>('http://localhost:8080/api/v1/locations?search=' + search + '&start=' + start, {
-            'location_types': location_types,
-            'attributes': attributes,
-            'noise': noise
-        });
-        return results
-    }
-    catch (error) {
+        body.coordinates_only = false;
+        const body_string = JSON.stringify(body);
+        const length = (new TextEncoder().encode(body_string)).length;
+        const request = new Request(url, {
+            method: 'POST',
+            body: body_string,
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Content-Length': length.toString(),
+            },
+        })
+        let results = await rawhttp<Location[]>(request);
+        //if (refresh) {
+        //    body.coordinates_only = true;
+        //    body.limit = 9999999;
+        //    const body_string = JSON.stringify(body);
+        //    const length = (new TextEncoder().encode(body_string)).length;
+        //    const request = new Request(url, {
+        //        method: 'POST',
+        //        body: body_string,
+        //        headers: {
+        //            'Content-Type': 'application/json; charset=UTF-8',
+        //            'Content-Length': length.toString(),
+        //        },
+        //    })
+        //    let pins: Coordinate[] = await rawhttp<Coordinate[]>(request);
+        //    store.pins = pins;
+        //}
+        if (refresh) {
+            store.results = results;
+        } else {
+            store.results = store.results.concat(results);
+        }
+    } catch (error) {
         console.log(error);
     }
 
